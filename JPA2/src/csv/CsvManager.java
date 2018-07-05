7495c53be38af525004215d50eefb796d3e821ff
stefano <stefano@stefano-HP-255-G5-Notebook-PC>
2018-07-05 15:45:47 +0200

    csv update

---
+++ JPA2/src/csv/CsvManager.java
@@ -0,0 +1,129 @@
+package csv;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+import model.Filamento;
+import model.Poscontorno;
+import model.PoscontornoPK;
+import model.Stella;
+
+public class CsvManager {
+	String path;
+
+	public CsvManager(String path) {
+		this.path = path;
+	}
+
+	public boolean uploadFile(int type) {
+		/*
+		 * tipo di file 0 file contorni filamento 1 file filamenti 2 file scheletro
+		 * filamenti 3 file stelle
+		 */
+		BufferedReader bufferedReader = null;
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA");
+		EntityManager em = emf.createEntityManager();
+
+		if (type > 3 || type < 0)
+			return false;
+		try {
+			bufferedReader = new BufferedReader(new FileReader(this.path));
+			bufferedReader.readLine(); // salta header
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		switch (type) {
+		case 0:
+			insertContorni(bufferedReader, em);
+		case 1:
+			//insertFilamenti(bufferedReader, em);
+		case 2:
+			//insertScheletro(bufferedReader, em);
+		case 3:
+			insertStelle(bufferedReader, em);
+		}
+
+		return true;
+	}
+
+	private void insertContorni(BufferedReader bufRead, EntityManager em) {
+		String line;
+		try {
+			while ((line = bufRead.readLine()) != null) {
+				String[] linePart = line.split(",");
+				int ID = Integer.parseInt(linePart[0].trim());
+				double lon = Double.parseDouble(linePart[1].trim());
+				double lat = Double.parseDouble(linePart[2].trim());
+				Filamento filamento = em.find(Filamento.class, ID);
+				PoscontornoPK pk = new PoscontornoPK();
+				pk.setIdfil(ID);
+				pk.setLatitudine(lat);
+				pk.setLongitudine(lon);
+				Poscontorno pos = em.find(Poscontorno.class, pk);
+				if (filamento != null && pos == null) {
+					Poscontorno posizione = new Poscontorno();
+					posizione.setFilamento(filamento);
+					posizione.setId(pk);
+					em.getTransaction().begin();
+					em.persist(posizione);
+
+					em.getTransaction().commit();
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void insertStelle(BufferedReader bufRead, EntityManager em) {
+		String line;
+		try {
+			while ((line = bufRead.readLine()) != null) {
+				String[] linePart = line.split(",");
+				int ID = Integer.parseInt(linePart[0].trim());
+				String nome = linePart[1].trim();
+				double lon = Double.parseDouble(linePart[2].trim());
+				double lat = Double.parseDouble(linePart[3].trim());
+				double flusso = Double.parseDouble(linePart[4].trim());
+				String tipologia = linePart[5].trim();
+				Stella stella = em.find(Stella.class, ID);
+				if (stella != null) {
+					Stella star = new Stella();
+					star.setId(ID);
+					star.setNome(nome);
+					star.setLongitudine(lon);
+					star.setLatitudine(lat);
+					star.setValoreflusso(flusso);
+					star.setTipologia(tipologia);
+					em.getTransaction().begin();
+					em.persist(stella);
+
+					em.getTransaction().commit();
+				} else {
+					// esiste-->aggiorno
+					em.getTransaction().begin();
+					stella.setId(ID);
+					stella.setNome(nome);
+					stella.setLongitudine(lon);
+					stella.setLatitudine(lat);
+					stella.setValoreflusso(flusso);
+					stella.setTipologia(tipologia);
+					em.getTransaction().commit();
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
